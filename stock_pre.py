d2971ca384680d70ca3bb647cbe91a77fea0a348
Aryan pratap <142568612+aryan151pratap@users.noreply.github.com>
2024-09-21 12:24:06 +0530

    Add files via upload

---
+++ stock_pre.py
@@ -0,0 +1,35 @@
+import joblib
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+file = "Stock.pkl"
+model = joblib.load(file)
+model1 = joblib.load("Stock1.pkl")
+
+st.title("Stock Prediction")
+
+#input
+o = st.number_input("Open ")
+h = st.number_input("High ")
+l = st.number_input("Low")
+v = st.number_input("Volume")
+
+
+dataframe = pd.DataFrame({
+	'Open':[o],
+	'High':[h],
+	'Low':[l],
+	'Volume':[v],
+	'high - low':[h - l],
+})
+
+# Prediction
+
+if st.button("Predict"):
+	close = model.predict(dataframe)
+	risk = model1.predict(dataframe)
+	st.write(close)
+	a = (risk>0.499).astype(int)
+	l = ["Not Risky", "Risky"]
+	st.write(l[int(a)])
